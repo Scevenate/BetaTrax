da759e8aebd754d2c1bc2fb9b8574d6ba85b5f32
Scevenate <scevenate@outlook.com>
2026-03-13 01:26:16 +0800

    Initial workspace setup

---
+++ BetaTrax/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class BetatraxConfig(AppConfig):
+    name = 'BetaTrax'
---
+++ scripts/up.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.." || exit 1
+uv run manage.py runserver
